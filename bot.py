504149bbc89cd0ad75dcec5945ae69620c65962a
q404p <75249588+q404p@users.noreply.github.com>
2022-02-11 00:21:01 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+        executor.start_polling(dp)
